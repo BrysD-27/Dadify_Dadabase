936fc66b5da28a67710b3d22299dee26ded1db7e
Jason Rader <jasonrader747@gmail.com>
2021-10-06 23:02:01 -0500

    Product Functions Ready For Code Review - Note: Need extra eyes on EditProductItem

--- db/product/product.js
+++ db/product/product.js
@@ -1,7 +1,13 @@
 const createProductItem = require("./productItems/createProductItem.js");
 const deleteProductItem = require("./productItems/deleteProductItem.js");
+const editProductItem = require("./productItems/editProductItem.js");
+const getAllProductItems = require("./productItems/getAllProductItems.js");
+const getProductItemById = require("./productItems/getProductItemById.js");
 
 module.exports = {
     createProductItem,
     deleteProductItem,
+    editProductItem,
+    getAllProductItems,
+    getProductItemById,
 }
