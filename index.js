0d935c97c7a9f188fc3ffaf3df86148e0acebcf5
Grady Blevins <grblevins2@gmail.com>
2021-10-20 19:51:12 -0500

    added api routes

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 const { PORT = 3000 } = process.env;
+
+const express = require('express');
+const server = express();
+
+const bodyParser = require('body-parser');
+server.use(bodyParser.json());
+
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
+server.use((req,res,next) => {
+    console.log("<_____Body Logger START_____");
+    console.log(req.body);
+    console.log("<_____Body Logger END_____>");
+
+    next();
+});
+
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
+
+const { client } = require('./db');
+client.connect();
+
+
 app.listen(PORT, () => {
     HTMLFormControlsCollection.log(`App listening on http://localhost:${PORT}`)
 });
