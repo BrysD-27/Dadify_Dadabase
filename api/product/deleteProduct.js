d78ead1bb6da6a0826f4097c12fca292b386e37f
Grady Blevins <grblevins2@gmail.com>
2021-10-29 19:34:12 -0500

    trying

--- api/product/deleteProduct.js
+++ api/product/deleteProduct.js
@@ -1,18 +1,18 @@
 const productsRouter = require('express').Router();
 const {getProductItemById, deleteProductItem} = require('../../db');
 
-productsRouter.delete('/:productId', async (req, res, next) => {
-    try {
-        const {id} = req.params.productId;
-        const product = await getProductItemById(id);
-        const deletedProduct = await deleteProductItem(product.id);
-        res.send(deletedProduct);
-        next();
-    } catch (error) {
-        console.error("Error deleting product item.");
-        throw error
-    };
-});
+// productsRouter.delete('/:productId', async (req, res, next) => {
+//     try {
+//         const {id} = req.params.productId;
+//         const product = await getProductItemById(id);
+//         const deletedProduct = await deleteProductItem(product.id);
+//         res.send(deletedProduct);
+//         next();
+//     } catch (error) {
+//         console.error("Error deleting product item.");
+//         throw error
+//     };
+// });
 
 module.exports = productsRouter;
 
--- api/product/patchProduct.js
+++ api/product/patchProduct.js
@@ -1,13 +1,13 @@
 const productsRouter = require('express').Router();
 const {updateProductItem, } = require('../../db');
 
-// // productsRouter.patch('/:productId', async (req, res, next) => {
-// //     const {product_id} = req.params.productId;
-// //     const {name, description, price} = req.body;
-// //     try {
-// //         const patchedProduct = await updateProductItem(product_id, name, description, price);
-// //         res.send({message: 'Success', patchedProduct})
-// //         next();
+// productsRouter.patch('/:productId', async (req, res, next) => {
+//     const {product_id} = req.params.productId;
+//     const {name, description, price} = req.body;
+//     try {
+//         const patchedProduct = await updateProductItem(product_id, name, description, price);
+//         res.send({message: 'Success', patchedProduct})
+//         next();
 
 //     } catch (error) {
 //         console.error("Error updating product.");
