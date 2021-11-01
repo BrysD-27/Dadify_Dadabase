63b45da370c315859ee28c96cbb0b2cb21f930c8
Jason Rader <jasonrader747@gmail.com>
2021-11-01 18:06:00 -0500

    register update.

--- api/users.js
+++ api/users.js
@@ -18,12 +18,12 @@ usersRouter.post('/register', async(req, res, next) => {
     try {
         const _user = await getUserByUsername(username);
         if(_user){
-        res.send({message: 'Username already taken, please select another!'})
+            res.send({message: 'Username already taken, please select another!'})
         } else if (_user.email){
-        res.send({message:'An account with this email is already in use. Maybe you wrote down the info somewhere?'})
-        } else { 
+            res.send({message:'An account with this email is already in use. Maybe you wrote down the info somewhere?'})
+        } 
         const user = await createUser({username, password, email});
-        res.send(user)}
+        res.send(user)
     } catch (error) {
         console.error(error);
     }
